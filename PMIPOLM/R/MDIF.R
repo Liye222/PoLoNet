0066cf20f10e2e5bf2e98d2de566b5194a706191
Liye222 <97601812+Liye222@users.noreply.github.com>
2022-05-14 10:39:59 +0800

    Delete PMIPOLM directory

--- PMIPOLM/R/MDIF.R
+++
@@ -1,6 +0,0 @@
-#---------MDIF-----------#
-MDIF <- function(data, EDGE) {
-    X <- (data[, EDGE[1]] - mean(data[, EDGE[1]])) * (data[, EDGE[2]] - mean(data[,
-        EDGE[2]]))
-    X
-}
--- PMIPOLM/R/RENAME.R
+++
@@ -1,5 +0,0 @@
-#----------RENAME-----------#
-RENAME <- function(varname, EDGE) {
-    a <- paste0(varname, EDGE[1], "_", EDGE[2])
-    a
-}
